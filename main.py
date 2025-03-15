272a6f3b630dac501dff3f394c9b200f26fe13ee
Mwakiogeofrey <mwakiogeofrey579@gmail.com>
2025-03-15 15:06:56 +0300

    fist commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def calculator():
+    num1 = float(input("Input num1: "))
+    num2 = float(input("Input num2: "))
+    operation = input("Input operation (+, -, *, /): ")
+
+    if operation == '+':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+    elif operation == '-':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+    elif operation == '*':
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid operation. Please enter one of +, -, *, or /.")
+
+calculator()
+
